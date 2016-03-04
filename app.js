4e4fb1a215f8dfdaa4e601c6648559b2ab761536
Henk Burgstra <henk.burgstra@gmail.com>
2016-03-04 14:41:00 +0100

    selectors zijn gevuld

--- app.js
+++ app.js
@@ -46,16 +46,57 @@ document.addEventListener('DOMContentLoaded', function() {
 	        m.addOption(leeftijdsgroep.id, leeftijdsgroep.title);
 	    }
 	}
-	m.addChangeHandler(onSelectionChanged);
-	m.render();
-	
+
 	var m = new multiselect("aspecten", "Aspecten", "Kies een aspect");
 	multiselects[m.id] = m;
-	m.addOption("A1", "A1 Spraakproductie");
-	m.addOption("A2", "A2 Auditieve verwerking");
-	m.addOption("A3", "A3 Grammaticaal-linguistische kennis");
-	m.addOption("A4", "A4 Lexicaal-semantische kennis");
-	m.addOption("A5", "A5 Pragmatiek");
-	m.addChangeHandler(onSelectionChanged);
-	m.render();
+	for (var i = 0; i < data.aspecten.available.length; i++) {
+	    var aspect = data.aspecten.available[i];
+	    if (aspect.id != "-") {
+    	    m.addOption(aspect.id, aspect.title);
+	    }
+	}
+
+	var m = new multiselect("taalbeheersing", "Taalbeheersing", "Kies de taalbeheersing");
+	multiselects[m.id] = m;
+	for (var i = 0; i < data.taalbeheersing.available.length; i++) {
+	    var taalbeheersing = data.taalbeheersing.available[i];
+	    if (taalbeheersing.id != "-") {
+	        m.addOption(taalbeheersing.id, taalbeheersing.title);
+	    }
+	}
+
+	var m = new multiselect("pakketten", "Pakketten", "Kies een pakket");
+	multiselects[m.id] = m;
+	for (var i = 0; i < data.pakketten.available.length; i++) {
+	    var pakket = data.pakketten.available[i];
+	    if (pakket.id != "-") {
+	        m.addOption(pakket.id, pakket.title);
+	    }
+	}
+
+	var m = new multiselect("cotan_kwalificatie", "COTAN-kwalificatie", "Kies een COTAN-kwalificatie");
+	multiselects[m.id] = m;
+	for (var i = 0; i < data.cotan_kwalificatie.available.length; i++) {
+	    var cotan_kwalificatie = data.cotan_kwalificatie.available[i];
+	    if (cotan_kwalificatie.id != "-") {
+	        m.addOption(cotan_kwalificatie.id, cotan_kwalificatie.title);
+	    }
+	}
+
+	var m = new multiselect("normering", "Normering", "Kies een normering");
+	multiselects[m.id] = m;
+	for (var i = 0; i < data.normering.available.length; i++) {
+	    var normering = data.normering.available[i];
+	    if (normering.id != "-") {
+	        m.addOption(normering.id, normering.title);
+	    }
+	}
+
+    for (var key in multiselects) {
+        if (multiselects.hasOwnProperty(key)) {
+            multiselects[key].addChangeHandler(onSelectionChanged);
+            multiselects[key].render();
+      }
+    }
+
 });
